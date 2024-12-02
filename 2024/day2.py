27d5e15d45d72cdcbdb34b37ffd66bc9f32874c0
deblina <deblina@uchicago.edu>
2024-12-02 12:49:22 -0500

    ordering was hard

--- 2024/day2.py
+++ 2024/day2.py
@@ -3,3 +3,24 @@ def read_input():
         return file.read().splitlines()
 
 data = read_input()
+
+
+# QUESTION 1 
+def is_good(xs): 
+    inc_or_dec = (xs == sorted(xs) or xs == sorted(xs, reverse=True))
+    ok = True 
+    for i in range(len(xs) - 1): 
+        diff = abs(xs[i] - xs[i + 1]) 
+        if not 1 <= diff <= 3: 
+            ok = False 
+    return inc_or_dec and ok  
+
+safe_reports_count = 0 
+
+for line in data:
+    level = list(map(int, line.split()))
+    if is_good(level): 
+        safe_reports_count += 1 
+    
+
+print(f"Total number of safe lines: {safe_reports_count}")
