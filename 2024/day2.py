d344a099cbc85466c2e97fb110a1e781f8536b27
deblina <deblina@uchicago.edu>
2024-12-02 13:00:18 -0500

    ok

--- 2024/day2.py
+++ 2024/day2.py
@@ -4,23 +4,33 @@ def read_input():
 
 data = read_input()
 
-
-# QUESTION 1 
-def is_good(xs): 
-    inc_or_dec = (xs == sorted(xs) or xs == sorted(xs, reverse=True))
-    ok = True 
-    for i in range(len(xs) - 1): 
-        diff = abs(xs[i] - xs[i + 1]) 
+# heavy assist from https://github.com/jonathanpaulson/AdventOfCode/blob/master/2024/2.py
+def is_good(line): 
+    inc_or_dec = (line == sorted(line) or line == sorted(line, reverse=True))
+    right_diff = True 
+    for i in range(len(line) - 1): 
+        diff = abs(line[i] - line[i + 1]) 
         if not 1 <= diff <= 3: 
-            ok = False 
-    return inc_or_dec and ok  
+            right_diff = False 
+    return inc_or_dec and right_diff  
 
 safe_reports_count = 0 
+safe_reports_one_removed = 0 
 
 for line in data:
     level = list(map(int, line.split()))
     if is_good(level): 
-        safe_reports_count += 1 
+        safe_reports_count += 1
+    
+    good = False 
+    for j in range(len(level)): 
+        tmp = level[:j] + level[j + 1:]
+        if is_good(tmp): 
+            good = True 
+    if good:
+        safe_reports_one_removed += 1 
     
 
 print(f"Total number of safe lines: {safe_reports_count}")
+print(f"Total number of safe lines if you could remove one: {safe_reports_one_removed}")
+
