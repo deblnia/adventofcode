483fa0445dedd51548670ea34910262fb6f71cad
deblina <deblina@uchicago.edu>
2024-12-03 12:29:35 -0500

    day3

--- 2024/day3.py
+++ 2024/day3.py
@@ -6,16 +6,20 @@ def read_input():
 
 data = read_input()
 
-# QUESTION 1 
 data_string = "\n".join(data)
 
+# part 2 assist from: https://github.com/mebeim/aoc/blob/master/2024/solutions/day03.py
+
+# matches = [(a, b, None, don't), (a, b, do, None) ... ]
 matches = re.findall(r"mul\((\d+),\s*(\d+)\)|(do\(\))|(don't\(\))", data_string)
 
 sum = 0
 sum2 = 0
 enabled = False
 for a, b, do, dont in matches:
+    # next is either enabled or disabled 
     if do or dont:
+        # change enabled based on whether do is present or not  
         enabled = bool(do)
     else:
         x = int(a) * int(b)
