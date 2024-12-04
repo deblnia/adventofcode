a1f2e17447544de1ca43bbee00b1f96c08c5e0c4
deblina <deblina@uchicago.edu>
2024-12-04 11:01:08 -0500

    i hate graphs

--- 2024/day4.py
+++ 2024/day4.py
@@ -6,6 +6,7 @@ data = read_input()
 def count_xmas_occurrences(grid):
     rows, cols = len(grid), len(grid[0])
     count = 0
+    count2 = 0 
 
     for r in range(rows):
         for c in range(cols):
@@ -32,7 +33,19 @@ def count_xmas_occurrences(grid):
                 count += 1
             if r - 3 >= 0 and c + 3 < cols and all(grid[r-i][c+i] == word for i, word in enumerate("SAMX")):
                 count += 1
+            
+            # QUESTION 2 - assist from https://github.com/jonathanpaulson/AdventOfCode/blob/master/2024/4.py
+            if r + 2 < rows and c + 2 < cols and grid[r][c] == 'M' and grid[r + 1][c + 1] == 'A' and grid[r + 2][c + 2] == 'S' and grid[r + 2][c] == 'M' and grid[r][c + 2] == 'S':
+                count2 += 1
+            if r + 2 < rows and c + 2 < cols and grid[r][c] == 'M' and grid[r + 1][c + 1] == 'A' and grid[r + 2][c + 2] == 'S' and grid[r + 2][c] == 'S' and grid[r][c + 2] == 'M':
+                count2 += 1
+            if r + 2 < rows and c + 2 < cols and grid[r][c] == 'S' and grid[r + 1][c + 1] == 'A' and grid[r + 2][c + 2] == 'M' and grid[r + 2][c] == 'M' and grid[r][c + 2] == 'S':
+                count2 += 1
+            if r + 2 < rows and c + 2 < cols and grid[r][c] == 'S' and grid[r + 1][c + 1] == 'A' and grid[r + 2][c + 2] == 'M' and grid[r + 2][c] == 'S' and grid[r][c + 2] == 'M':
+                count2 += 1
+    print(f"p1: {count}")
+    print(f"p2: {count2}")
 
     return count
 grid = [list(row) for row in data]
-print(f"{count_xmas_occurrences(grid)}")
+count_xmas_occurrences(grid)
