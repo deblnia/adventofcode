177603615913c6cd6f2165566251b588f077f21e
deblina <deblina@uchicago.edu>
2024-12-07 11:26:20 -0500

    p2

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,4 +1,5 @@
 from itertools import product
+from tqdm import tqdm
 
 def get_input(path): 
     with open(path, "r") as file: 
@@ -13,8 +14,7 @@ def get_input(path):
         d[target] = rr
     return d
 
-def try_all_ops(target, values):
-    ops = ['+', '*']
+def try_all_ops(target:int, values:list, ops:list[str]):
     # generate all combos of ops for the number of gaps 
     for operations in product(ops, repeat=len(values)-1):
         start = values[0]
@@ -23,18 +23,23 @@ def try_all_ops(target, values):
                 start += values[i+1]
             elif op == '*':
                 start *= values[i+1]
+            elif op == "||": 
+                start = int(str(start) + str(values[i +1]))
         if start == target:
             return target
     return 0
 
 
-total_part1 = 0
-total_part2 = 0
+if __name__ == "__main__": 
+    total_part1 = 0
+    total_part2 = 0
 
-data = get_input("inputs/day7.txt")
+    data = get_input("inputs/day7.txt")
 
-for target, nums in data.items():
-    goal = int(target)
-    total_part1 += try_all_ops(goal, nums)
+    for target, nums in tqdm(data.items()):
+        goal = int(target)
+        total_part1 += try_all_ops(goal, nums, ['+', '*'])
+        total_part2 += try_all_ops(goal, nums, ['+', '*', '||'])
 
-print(f"Part 1: {total_part1}")
+    print(f"Part 1: {total_part1}")
+    print(f"Part 2: {total_part2}")
