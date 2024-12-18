f6b89148bd9ebf0eceaa680abfc3334eb1bef946
deblina <deblina@uchicago.edu>
2024-12-17 22:44:10 -0500

    ok

--- 2024/day15.py
+++ 2024/day15.py
@@ -50,5 +50,59 @@ p1 = sum(
 
 print(f"P1: {p1}")
 
-p2 = ""
+# roooough. heavily borrowed from https://gitlab.com/0xdf/aoc2024/-/blob/main/day15/day15.py?ref_type=heads
+grid_map = {"#": "##", "O": "[]", ".": "..", "@": "@."}
+with open("inputs/day15.txt", "r") as f:
+    grid_str, moves = f.read().split("\n\n")
+grid = []
+for row in grid_str.splitlines():
+    new_row = []
+    for val in row:
+        new_row.extend(grid_map[val])
+    grid.append(new_row)
+
+for r, row in enumerate(grid):
+    for c, val in enumerate(row):
+        if val == "@":
+            break
+    else:
+        continue
+    break
+
+move_map = {"<": (0, -1), "^": (-1, 0), ">": (0, 1), "v": (1, 0)}
+for move in moves.replace("\n", ""):
+    dr, dc = move_map[move]
+    keep_moving = True
+    to_move = [(r, c)] # need to keep track of the starting
+    i = 0
+    while i < len(to_move):
+        rr, cc = to_move[i]
+        i += 1
+        nr, nc = rr + dr, cc + dc
+        if (nr, nc) in to_move:
+            continue
+        if grid[nr][nc] == "#":
+            do_move = False
+            break
+        if grid[nr][nc] == ".":
+            continue
+        if grid[nr][nc] == "[":
+            to_move.extend([(nr, nc), (nr, nc + 1)])
+        elif grid[nr][nc] == "]":
+            to_move.extend([(nr, nc), (nr, nc - 1)])
+        else:
+            assert False
+
+    if not keep_moving:
+        continue
+    grid_copy = [list(row) for row in grid]
+    r, c = r + dr, c + dc
+    for rr, cc in to_move:
+        grid[rr][cc] = "."
+    for rr, cc in to_move:
+        grid[rr + dr][cc + dc] = grid_copy[rr][cc]
+
+p2 = sum(
+    r * 100 + c for r, row in enumerate(grid) for c, val in enumerate(row) if val == "["
+)
 print(f"Part 2: {p2}")
