798f9d43bc38fea35a8cd8d9373feb16a70afe73
deblina <deblina@uchicago.edu>
2024-12-19 22:23:25 -0500

    day17 all

--- 2024/day17.py
+++ 2024/day17.py
@@ -1,8 +1,10 @@
 import re
+import math 
+
 with open("inputs/day17.txt", "r") as f: 
     a,b, c, *program = list(map(int, re.findall(r"\d+", f.read())))
 
-def eval(a: int, b: int = 0, c: int = 0) -> list[int]:
+def eval(a, b, c):
     def combo(val: int) -> int:
         assert val != 7, "Invalid combo value"
         if val <= 3:
@@ -40,5 +42,22 @@ def eval(a: int, b: int = 0, c: int = 0) -> list[int]:
 p1 = ",".join(map(str, eval(a,b,c)))
 print(f"P1: {p1}")
 
-# p1 = ",".join(map(str, eval(a,b,c)))
-# print(f"P1: {p1}")
+# quine! 
+def part2(a, b , c, program):
+    queue = [(0, len(program) - 1)]
+    min = math.inf
+    while len(queue) > 0:
+        potential_a, i = queue.pop()
+        for j in range(8): # check all opcodes 
+            a = potential_a + j * 8 ** i
+            out = eval(a, b, c)
+            if len(out) > i and out[i] == program[i]:
+                if i == 0:
+                    if a < min:
+                        min = a
+                else:
+                    queue.append((a, i - 1))
+    return min
+
+p2 = part2(a, b, c, program)
+print(f"P2: {p2}")
