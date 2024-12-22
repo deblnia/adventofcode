50520bb3dc09f704a71452f898f238bdd1792f90
deblina <deblina@uchicago.edu>
2024-12-21 22:22:04 -0500

    p2

--- 2024/day18.py
+++ 2024/day18.py
@@ -10,6 +10,7 @@ with open("inputs/day18.txt", "r") as f:
 
 def simulate(num_iterations): 
     # BFS through GRID_DIM x GRID_DIM grid 
+    # DFS would require route comparisons, BFS does comps as it goes  
     q = deque([(0, 0, 0)]) # x, y, num_moves 
     visited = set()
 
@@ -32,6 +33,55 @@ def simulate(num_iterations):
                 q.append((new_x, new_y, num_moves + 1))
     return -1 
 
-part1 = simulate(1024)
-print(f"Part 1: {part1}")
+p1 = simulate(1024)
+print(f"P1: {p1}")
+
+GRID_DIM = 70 + 1 
+
+def is_path_exists(falling_bytes, num_iterations):
+    q = deque([(0, 0, 0)])  
+    visited = set()
+    current_bytes = set(falling_bytes[:num_iterations]) # O(1) lookup
+    
+    while q:
+        x, y, num_moves = q.popleft()
+        if (x, y) in visited:
+            continue
+        
+        visited.add((x, y))
+        
+        if x == GRID_DIM - 1 and y == GRID_DIM - 1:
+            return True
+            
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            new_x, new_y = x + dx, y + dy
+            if (
+                0 <= new_x < GRID_DIM 
+                and 0 <= new_y < GRID_DIM
+                and (new_x, new_y) not in current_bytes
+            ):
+                q.append((new_x, new_y, num_moves + 1))
+    
+    return False
+
+def find_blocking_byte(falling_bytes):
+    left = 1
+    right = len(falling_bytes)
+    # binary search bytes to find blocking 
+    while left < right:
+        mid = (left + right) // 2
+        if is_path_exists(falling_bytes, mid):
+            left = mid + 1
+        else:
+            right = mid
+    
+    # left = index of the first byte that blocks the path
+    if is_path_exists(falling_bytes, left):
+        return None  # No blocking byte found
+    return left, falling_bytes[left-1]
+
+
+p2_index, p2_coords = find_blocking_byte(falling_bytes)
+print(f"P2: {p2_coords}")
+
 
